e42871bb2a91c22cd01e71a7108d87d5c927816f
Mayeda Kobo <wow.kobo530@gmail.com>
2017-09-11 03:02:52 +0900

    Add favicon.ico

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Angular4 + Saas API integration Project 
+# Angular and Saas API Integration Project
+
+1. Run `npm install` to install app dependencies (you may see some warnings due to @ngtools/webpack and webpack...ignore them)
+
+2. Run `npm start` to start the server and launch the app
+
+
+
---
+++ src/app/app.routing.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var router_1 = require("@angular/router");
+var app_routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/someRoute' }
+];
+exports.app_routing = router_1.RouterModule.forRoot(app_routes);
+//# sourceMappingURL=app.routing.js.map
